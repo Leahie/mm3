9e3f019a0143dd56fdef2ecba90d8b144fad59c5
Leah Zhang <LeahZhang1595995@gmail.com>
2024-02-04 13:30:56 -0500

    New data

--- index.py
+++ index.py
@@ -9,6 +9,7 @@ FS = (16, 9)
 
 df = pd.read_csv('./world_population_by_year_1950_2023.csv')
 
+
 df2 = df.loc[:, '1950': '2023':1]
 
 sums = df2.sum().rename('total')
@@ -24,29 +25,26 @@ plt.ylabel('Population')
 plt.title('Global Population Since 1950')
 plt.show() """
 
-def normalize(arr, t_min, t_max):
+def normalize(arr, t_min, t):
     norm_arr = []
-    diff = t_max - t_min
-    diff_arr = max(arr) - min(arr)    
+    diff = t - t_min
+    diff_arr =(arr) - min(arr)    
     for i in arr:
         temp = (((i - min(arr))*diff)/diff_arr) + t_min
         norm_arr.append(temp)
     return norm_arr
 
-def plot_ratios(data, pop_min = 0, pop_max = None):
+def plot_ratios(data, pop_min = 0, pop = None):
     # proportional growth rate with respect to D 
     data = data[data>pop_min]
-    if pop_max is not None: data = data[data<pop_max]
+    if pop is not None: data = data[data<pop]
     slopes = 0.5 *(data.diff(1)-data.diff(-1))
     ratios = slopes/ data
     x = data.values[1:-1] # returns array of data 
     y = ratios.values[1:-1] # returns array from ratios
     return x, y
-""" fig = plt.figure(figsize=(8,8))
-    ax = fig.add_subplot(1,1,1)
-    plt.plot(x, y, 'o')
-    ax.set_xlabel('D(t)')
-    ax.set_ylabel('Ratios of slopes to function values') """
+""" fig = plt.figure(figsize=(8,8)) = fig.add_subplot(1,1,1)
+    plt.plot(x, y, 'o').set_xlabel('D(t)').set_ylabel('Ratios of slopes to function values') """
     
 
 def linear_regression(x,y):
@@ -57,52 +55,58 @@ def linear_regression(x,y):
     a = reg.coef_[0]
     b = reg.intercept_
     y_hat = a*x + b
-
-    return a, b
-"""     fig = plt.figure(figsize=(8,8))
+    fig = plt.figure(figsize=(8,8))
     ax = fig.add_subplot(1,1,1) 
     plt.plot(x,y, 'o')
     plt.plot(x, y_hat, '-', label='Linear Reg')
     ax.set_xlabel('D(t)')
     ax.set_ylabel('Ratios of slopes to function values')
-    ax.legend() """
+    ax.legend()
+    return a, b
+"""      """
 
-def t(a, b, data, pop_min=0, pop_max=None, t0=0):
+def t(a, b, data, pop_min=0, pop=None, t0=0):
     # find the best value based on the Lin_Reg
     k =  b
     L = -b/a
     data = data[data>pop_min]
-    if pop_max is not None: data = data[data<pop_max]
-    logfunc = 2.2*10**9 + L/ (1.0 + np.exp(-k*(np.arange(len(data))-t0)))  
-    fig = plt.figure(figsize=(8,8))
-    ax = fig.add_subplot(1,1,1) 
-    plt.plot(logfunc, 'o')
-    plt.plot(data.values, 'd')
+    if pop is not None: data = data[data<pop]
+    logfunc = L/ (1.0 + np.exp(-k*(np.arange(len(data))-t0)))  
     return L, k
+""" fig = plt.figure(figsize=(8,8)) = fig.add_subplot(1,1,1) 
+    plt.plot(logfunc, 'o', label = "logfunc")
+    plt.plot(data.values, 'd').legend()     """
 
-def extended_plot(data, pop_min, L, k, t0, years_before = 50, years_after = 30, figsize = (16, 9) ):
+
+def extended_plot(data, L, k, t0, pop_min =0, years_before = 100, years_after = 200, figsize = (16, 9) ):
     # plot log function against original data 
     data_org = data.copy(deep = True)
     data = data[data>pop_min]
     data = data.to_frame('Total')
     data_start, data_end = data.index.min(), data.index.max()
+
     start = int(data_start) - int(years_before)
     end = int(data_end) + int(years_after )
-    ix = pd.date_range(start=start, end=end, freq = 'Y')
 
+    data = data.reindex(range(start, end))
     data['idx'] = np.arange(len(data))
+    data['idx'] -= data.loc[int(data_start), 'idx'] # normalizing 
+    data['logistic'] = L/ (1.0 + np.exp(-k*(data['idx'].values-t0)))
+    ax = data['logistic'].plot(figsize=figsize, logy=False, label="log function")
 
-    print(data)
+    data_org['idx'] = np.arange(1950, 2024)
+    print(times_X, pop_Y)
+    _ = plt.plot(times_X, pop_Y, "d", alpha=0.5, label = "Original Data")
+    plt.xlabel('Time')
+    plt.ylabel('Population')
+    plt.title('Predicted Global Population From 1850-2223')
 
-    data['idx'] -= data.loc[data_start, 'idx'] # normalizing 
-    data['logistic'] = L/ (1.0 + np.exp(-k*(np.arange(len(data))-t0)))
-    ax = data['logistic'].plot(figsize=figsize, logy=False)
-    _ = data[start:].plot(ax=ax, style='o')
 
 data = s['total']
-x,y = plot_ratios(data, 7*10**9)
+x,y = plot_ratios(data)
 a,b = linear_regression(x,y)
-t0 = 1.3
-L,k = t(a,b, data, 7*10**9,t0=t0 )
-extended_plot(data, 7*10**9, L, k, t0, figsize=FS)
+t0 = 60
+L,k = t(a,b, data, t0=t0 )
+print(L, k)
+#extended_plot(data, L=L, k=k, t0=t0, figsize=FS)
 plt.show()
